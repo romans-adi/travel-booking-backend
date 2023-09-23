555240225113c7bd68d5b1d48a989db85190ce55
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-09-23 22:40:57 +0300

    update User  model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :travels, dependent: :destroy
+  has_many :travels, through: :reservations
   has_many :reservations
 
   validates :name, presence: true
