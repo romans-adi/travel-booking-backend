d9001767fbb1fc26d2de3d6a7dd0ac93da86870a
Romans S <romans.spilaks@gmail.com>
2023-09-23 07:16:37 +0300

    Add paths in the routes under the namespace api/v1. Create empty controllers to handle requests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace 'api' do
+    namespace 'v1' do
+      resources :places, only: [:index, :show]
+      resources :travels, only: [:index, :show]
+      resources :reservations, only: [:index, :create, :destroy]
+      resources :users, only: [:index, :show]
+    end
+  end
 end
