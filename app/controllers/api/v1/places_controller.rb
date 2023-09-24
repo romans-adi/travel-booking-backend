0ae105253eb74132bccab69f97d8df1115be2210
Romans S <romans.spilaks@gmail.com>
2023-09-24 20:32:58 +0300

    Add includes to Places controller for travel based on model's relation Place which has one Travel.

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::PlacesController < ApplicationController
     @places = Place.all
     render json: @places
   end
+
+  def show
+    @place = Place.includes(:travel).find(params[:id])
+    render json: @place
+  end
 end
