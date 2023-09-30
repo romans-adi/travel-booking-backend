1d98eefb85efe539273b073833717b7a2490de78
Romans S <romans.spilaks@gmail.com>
2023-09-30 20:44:03 +0300

    Update seeds to correctly provide seeds data (Travel data)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 places_data = [
   {
-    name: 'Explore the Charm of Paris',
+    name: 'Paris',
     main_picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/800px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg',
     description: 'Discover the cultural and architectural wonders of the City of Light',
   },
@@ -12,42 +12,42 @@ places_data = [
     description: 'Hit the slopes in the Swiss Alps',
   },
   {
-    name: 'Beach Paradise',
+    name: 'Maldives',
     description: 'Relax on the sandy beaches of the Maldives',
     main_picture: 'https://www.hollywoodreporter.com/wp-content/uploads/2023/03/Maldives-ArrivalJetty-9-H-2023.jpg?w=1024',
   },
   {
-    name: 'Historical Athens',
+    name: 'Athens',
     description: 'Explore the ancient wonders of Athens',
     main_picture: 'https://cdn.britannica.com/66/102266-050-FBDEFCA1/acropolis-city-state-Greece-Athens.jpg',
   },
   {
-    name: 'Safari Adventure',
+    name: 'Kenyan Safari',
     description: 'Discover wildlife on a Kenyan safari',
     main_picture: 'https://i0.wp.com/relaxaroundtheworld.com/wp-content/uploads/2021/10/Kenya-Safari.jpg?fit=1440%2C700&ssl=1',
   },
   {
-    name: 'Mountain Retreat',
+    name: 'The Rockies',
     description: 'Escape to the tranquility of the Rockies',
     main_picture: 'https://lp-cms-production.imgix.net/2021-12/GettyImages-1136325182.jpg',
   },
   {
-    name: 'Tropical Rainforest',
+    name: 'Amazon Rainforest',
     description: 'Experience the lush beauty of the Amazon',
     main_picture: 'https://files.adventure-life.com/12/45/06/iStock-CR-hikerrainforest/1300x820.webp',
   },
   {
-    name: 'Island Paradise',
+    name: 'Bali',
     description: 'Relax in the pristine waters of Bali',
     main_picture: 'https://balidave.com/wp-content/uploads/2022/11/best-hotel-bali.jpeg',
   },
   {
-    name: 'City of Lights',
+    name: 'Tokyo',
     description: 'Marvel at the vibrant energy of Tokyo',
     main_picture: 'https://i0.wp.com/touristjourney.com/wp-content/uploads/2020/02/jezael-melgoza-layMbSJ3YOE-unsplash-scaled.jpg?fit=2560%2C1661&ssl=1',
   },
   {
-    name: 'Desert Adventure',
+    name: 'Sahara Desert',
     description: 'Explore the dunes of the Sahara',
     main_picture: 'https://www.intrepidtravel.com/adventures/wp-content/uploads/2017/05/FU8A0260-88x450.jpg',
   },
@@ -56,8 +56,10 @@ places_data = [
 places_data.each do |place_data|
   place = Place.create(place_data)
     case place.name
-    when 'Explore the Charm of Paris'
+    when 'Paris'
       travel = Travel.create(
+        name: 'Explore the Charm of Paris',
+        image: 'https://img.freepik.com/free-photo/eiffel-tower-as-famous-city-landmark-paris_649448-4835.jpg?w=360',
         price: Faker::Number.decimal(l_digits: 2),
         trip_duration: Faker::Number.between(from: 1, to: 15),
         group_size: Faker::Number.between(from: 1, to: 20),
@@ -67,6 +69,8 @@ places_data.each do |place_data|
       )
     when 'Swiss Alps'
       travel = Travel.create(
+        name: 'Alpine Adventure Escapes',
+        image: 'https://i.pinimg.com/736x/1f/e7/8f/1fe78fdf26caea2a98a4a9c8872efa60.jpg',
         price: Faker::Number.decimal(l_digits: 2),
         trip_duration: Faker::Number.between(from: 1, to: 15),
         group_size: Faker::Number.between(from: 1, to: 20),
@@ -74,8 +78,10 @@ places_data.each do |place_data|
         travel_type: 'skiing',
         place_id: place.id
       )
-  when 'Beach Paradise'
+  when 'Maldives'
     travel = Travel.create(
+      name: 'Beach Paradise',
+      image: 'https://i0.hippopx.com/photos/140/199/675/veligandu-island-maldives-ocean-island-preview.jpg',
       price: Faker::Number.decimal(l_digits: 2),
       trip_duration: Faker::Number.between(from: 1, to: 15),
       group_size: Faker::Number.between(from: 1, to: 20),
@@ -83,8 +89,10 @@ places_data.each do |place_data|
       travel_type: 'beach',
       place_id: place.id
     )
-  when 'Historical Athens'
+  when 'Athens'
     travel = Travel.create(
+      name: 'Ancient Athenian Odyssey',
+      image: 'https://images.unsplash.com/photo-1605153322277-dd0d7f608b4d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXRoZW5zfGVufDB8fDB8fHww&w=1000&q=80',
       price: Faker::Number.decimal(l_digits: 2),
       trip_duration: Faker::Number.between(from: 1, to: 15),
       group_size: Faker::Number.between(from: 1, to: 20),
@@ -92,8 +100,10 @@ places_data.each do |place_data|
       travel_type: 'history',
       place_id: place.id
     )
-  when 'Safari Adventure'
+  when 'Kenyan Safari'
     travel = Travel.create(
+      name: 'Savannah Serenity Safaris',
+      image: 'https://ivhq.imgix.net/images/map/ivhq-volunteer-abroad-region-africa-hero.jpg?auto=format,compress',
       price: Faker::Number.decimal(l_digits: 2),
       trip_duration: Faker::Number.between(from: 1, to: 15),
       group_size: Faker::Number.between(from: 1, to: 20),
@@ -101,8 +111,10 @@ places_data.each do |place_data|
       travel_type: 'safari',
       place_id: place.id
     )
-  when 'Mountain Retreat'
+  when 'The Rockies'
     travel = Travel.create(
+      name: 'Rocky Mountain Explorer Expeditions',
+      image: 'https://live.staticflickr.com/1584/26413306660_626c3d4e21_b.jpg',
       price: Faker::Number.decimal(l_digits: 2),
       trip_duration: Faker::Number.between(from: 1, to: 15),
       group_size: Faker::Number.between(from: 1, to: 20),
@@ -110,8 +122,10 @@ places_data.each do |place_data|
       travel_type: 'mountain',
       place_id: place.id
     )
-  when 'Tropical Rainforest'
+  when 'Amazon Rainforest'
     travel = Travel.create(
+      name: 'Tropical Rainforest Treks',
+      image: 'https://images.unsplash.com/photo-1599582964755-971498d2b4a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YW1hem9uJTIwZm9yZXN0fGVufDB8fDB8fHww&w=1000&q=80',
       price: Faker::Number.decimal(l_digits: 2),
       trip_duration: Faker::Number.between(from: 1, to: 15),
       group_size: Faker::Number.between(from: 1, to: 20),
@@ -119,8 +133,10 @@ places_data.each do |place_data|
       travel_type: 'rainforest',
       place_id: place.id
     )
-  when 'Island Paradise'
+  when 'Bali'
     travel = Travel.create(
+      name: 'Bali Bliss Escapes',
+      image: 'https://images.unsplash.com/photo-1616484173745-07f25fd0547f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFsaSUyQyUyMGluZG9uZXNpYXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',
       price: Faker::Number.decimal(l_digits: 2),
       trip_duration: Faker::Number.between(from: 1, to: 15),
       group_size: Faker::Number.between(from: 1, to: 20),
@@ -128,8 +144,10 @@ places_data.each do |place_data|
       travel_type: 'beach',
       place_id: place.id
     )
-  when 'City of Lights'
+  when 'Tokyo'
     travel = Travel.create(
+      name: 'City of Lights Tranquil Journeys',
+  image: 'https://images.unsplash.com/photo-1503899036084-c55cdd92da26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dG9reW98ZW58MHx8MHx8fDA%3D&w=1000&q=80',
       price: Faker::Number.decimal(l_digits: 2),
       trip_duration: Faker::Number.between(from: 1, to: 15),
       group_size: Faker::Number.between(from: 1, to: 20),
@@ -137,8 +155,10 @@ places_data.each do |place_data|
       travel_type: 'city',
       place_id: place.id
     )
-  when 'Desert Adventure'
+  when 'Sahara Desert'
     travel = Travel.create(
+      name: 'Sands Expeditions',
+      image: 'https://i.pinimg.com/736x/82/bc/bd/82bcbd37e159cfc3251929c629f8b835.jpg',
       price: Faker::Number.decimal(l_digits: 2),
       trip_duration: Faker::Number.between(from: 1, to: 15),
       group_size: Faker::Number.between(from: 1, to: 20),
