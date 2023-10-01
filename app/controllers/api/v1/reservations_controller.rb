0c2bd5a08517e985093af75c0e745d57d3c874a1
Romans S <romans.spilaks@gmail.com>
2023-10-02 01:20:43 +0300

    Bring back first line of Reservation controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,33 +1,35 @@
-before_action :authenticate_user!
+class Api::V1::ReservationController < ApplicationController
+  before_action :authenticate_user!
 
-def index
-  @reservations = current_user.reservations
-  render json: @reservations
-end
-
-def show
-  @reservation = Reservation.find(params[:id])
-  render json: @reservation
-end
-
-def create
-  @reservation = Reservation.new(reservation_params)
-  @reservation.user_id = current_user.id
-  if @reservation.save
-    render json: @reservation, status: :created
-  else
-    render json: @reservation.errors, status: :unprocessable_entity
+  def index
+    @reservations = current_user.reservations
+    render json: @reservations
   end
-end
 
-def destroy
-  @reservation = Reservation.find(params[:id])
-  @reservation.destroy
-  head :no_content
-end
+  def show
+    @reservation = Reservation.find(params[:id])
+    render json: @reservation
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user_id = current_user.id
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: @reservation.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    head :no_content
+  end
 
   private
 
-def reservation_params
-  params.require(:reservation).permit(:date_of_reservation, :city, :user_id, :travel_id)
+  def reservation_params
+    params.require(:reservation).permit(:date_of_reservation, :city, :user_id, :travel_id)
+  end
 end
