99ca28bdbf5bb433476cc8bfdaed6f8ce719c7f8
Romans S <romans.spilaks@gmail.com>
2023-10-01 06:28:44 +0300

    Lintering

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,35 +1,33 @@
-class Api::V1::ReservationsController < ApplicationController
-  before_action :authenticate_user!
+before_action :authenticate_user!
 
-  def index
-    @reservations = current_user.reservations
-    render json: @reservations
-  end
+def index
+  @reservations = current_user.reservations
+  render json: @reservations
+end
 
-  def show
-    @reservation = Reservation.find(params[:id])
-    render json: @reservation
-  end
+def show
+  @reservation = Reservation.find(params[:id])
+  render json: @reservation
+end
 
-  def create
-    @reservation = Reservation.new(reservation_params)
-    @reservation.user_id = current_user.id
-    if @reservation.save
-      render json: @reservation, status: :created
-    else
-      render json: @reservation.errors, status: :unprocessable_entity
-    end
+def create
+  @reservation = Reservation.new(reservation_params)
+  @reservation.user_id = current_user.id
+  if @reservation.save
+    render json: @reservation, status: :created
+  else
+    render json: @reservation.errors, status: :unprocessable_entity
   end
+end
 
-  def destroy
-    @reservation = Reservation.find(params[:id])
-    @reservation.destroy
-    head :no_content
-  end
+def destroy
+  @reservation = Reservation.find(params[:id])
+  @reservation.destroy
+  head :no_content
+end
 
   private
 
-  def reservation_params
-    params.require(:reservation).permit(:date_of_reservation, :city, :user_id, :travel_id)
-  end
+def reservation_params
+  params.require(:reservation).permit(:date_of_reservation, :city, :user_id, :travel_id)
 end
