7529c2a32fd55fe5fb2025a421079fd4c7163334
Daniel <carreraprogrammer@gmail.com>
2023-10-05 09:42:14 -0500

    Add unit test for user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :role, presence: true
+  validates :password, presence: true, confirmation: true
 
   enum role: %i[user agency]
   after_initialize :set_default_role, if: :new_record?
